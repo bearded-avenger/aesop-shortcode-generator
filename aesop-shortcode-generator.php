c413c103b976b0d22cc8dc64622d8aa18cd4e74f
Bearded Avenger <email@nickhaskins.com>
2013-11-26 16:00:18 -0600

    final

--- aesop-shortcode-generator.php
+++ aesop-shortcode-generator.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Aesop Shortcode Generator
-Plugin URI: http://nickhaskins.co
-Description: A Wordpress shortcodes plugin built exclusively for PageLines DMS
+Plugin URI: http://nickhaskins.co/products/aesop
+Description: A shortcode generator built exclusively to work with Aesop visual story telling Wordpress theme.
 Version: 3.0
-Author: Bearded Avenger
+Author: Nick Haskins
 Author URI: http://nickhaskins.co
 */
 
@@ -13,7 +13,6 @@ class aesopSCGen {
 	public function __construct(){
 		require_once('libs/load.php' );
 		require_once('libs/available.php' );
-		require_once('libs/the_shortcodes.php' );
 	}
 }
 new aesopSCGen;
--- libs/available.php
+++ libs/available.php
@@ -45,8 +45,104 @@
 				),
 				'content' => __( 'Caption', 'ba-shortcodes' ),
 				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Choose an icon','aesop-shortcodes' )
+				'desc' => __( 'Parallax styled image component with caption and optional lightbox.','aesop-shortcodes' )
 			),
+			'quote' => array(
+				'name' => 'Aesop Quote Section',
+				'type' => 'wrap',
+				'atts' => array(
+					'background' => array(
+						'values' => array( ),
+						'default' => '#282828',
+						'desc' => __( 'Hex Color of Background', 'aesop-shortcodes' )
+					),
+					'text' => array(
+						'values' => array(),
+						'default' => '#FFFFFF',
+						'desc' => __('Hex Color of Text', 'aesop-shortcodes' )
+					),
+					'height' => array(
+						'values' => array(),
+						'default' => '',
+						'desc' => __('Height of Image Area', 'aesop-shortcodes' )
+					),
+				),
+				'content' => __( 'Quote Here', 'ba-shortcodes' ),
+				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' => __( 'Section quote area with background and color controls.','aesop-shortcodes' )
+			),
+			'image_back' => array(
+				'name' => 'Image Back',
+				'type' => 'wrap',
+				'atts' => array(
+					'img' => array(
+						'values' => array( ),
+						'default' => '#',
+						'desc' => __( 'Image URL', 'aesop-shortcodes' )
+					),
+					'color' => array(
+						'values' => array(),
+						'default' => '#FFFFFF',
+						'desc' => __('Color of Text', 'aesop-shortcodes' )
+					)
+				),
+				'content' => __( 'Your normal content goes here.', 'ba-shortcodes' ),
+				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' => __( 'Creates a background image section, with text in the middle.','aesop-shortcodes' )
+			),
+			'content_cols' => array(
+				'name' => 'Magazine Columns',
+				'type' => 'wrap',
+				'atts' => array(
+					'num' => array(
+						'values' => array( ),
+						'default' => 2,
+						'desc' => __( 'Number of Columns', 'aesop-shortcodes' )
+					)
+				),
+				'content' => __( 'All your normal text goes here.', 'ba-shortcodes' ),
+				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' => __( 'Wraps your text in magazine style columns.','aesop-shortcodes' )
+			),
+			'chapter_head' => array(
+				'name' => 'Chapter Heading',
+				'type' => 'single',
+				'atts' => array(
+					'num' => array(
+						'values' => array( ),
+						'default' => 1,
+						'desc' => __( 'What Chapter is This?', 'aesop-shortcodes' )
+					)
+				),
+				'content' => __( 'Null here', 'ba-shortcodes' ),
+				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' => __( 'Creates the scroll to point, as a chapter heading.','aesop-shortcodes' )
+			),
+			'video_sc' => array(
+				'name' => 'Video Section',
+				'type' => 'single',
+				'atts' => array(
+					'src' => array(
+						'values' => array(
+							'vimeo',
+							'youtube',
+							'kickstarter',
+							'viddler',
+							'dailymotion'
+						),
+						'default' => 'vimeo',
+						'desc' => __('Video Source', 'aesop-shortcodes' )
+					),
+					'id' => array(
+						'values' => array( ),
+						'default' => '',
+						'desc' => __( 'Video ID', 'aesop-shortcodes' )
+					)
+				),
+				'content' => __( 'Null here', 'ba-shortcodes' ),
+				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' => __( 'Creates a video section.','aesop-shortcodes' )
+			)
 		);
 
 		if ( $shortcode )
